698870c13d5abc0c1c711b6ea906f96195969df3
SokolowaEkaterina <soko20wa.ek0ter1na@gmail.com>
2020-07-29 21:41:09 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/*ACORDION////////////////////////////////////////////*/
+$(document).ready(function() {
+    $('.accordion-item-trigger').click(function(){
+        /*$(this).next('.accordion-item-content').slideToggle(200);*/
+        $(this).parent('.accordion-item').toggleClass('accordion-item--active');
+    });
+});
+
+
+/*/BUTTON-TO-TOP//////////////////////////////////////////////////*/
+function backToTop() {
+    let button = $ ('.button-to-top');
+
+    $(window).on('scroll', () => {
+        if ($(this).scrollTop() >= 100) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        $('html').animate({scrollTop: 0}, 1000);
+    })
+}
+
+backToTop();
+
+
+/*/MOBILE-NAVIGATION////////////////////////////////////////////*/
+const menuToggle = document.querySelector('#menu_togle');
+const mobileNavContainer = document.querySelector('#mobile-nav');
+
+
+menuToggle.onclick = function(){
+    menuToggle.classList.toggle('menu-icon-active');
+    mobileNavContainer.classList.toggle('mobile-nav--active'); 
+}
